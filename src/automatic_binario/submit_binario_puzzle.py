0738eda4ccd0cfac56b65193e6fa0a9918f22d1f
van <66967826+vannytrinh@users.noreply.github.com>
2025-12-04 04:30:42 +0000

    Fix object names

--- src/automatic_binario/submit_binario_puzzle.py
+++ src/automatic_binario/submit_binario_puzzle.py
@@ -5,11 +5,11 @@ from binario_submitters import PuzzleBinarioSubmitter
 if __name__ == "__main__":
     API_TOKEN = "API-TOKEN HERE"
 
-    solver = BinarioPuzzleSolver()
-    auto_solver = BinarioAutomation(API_TOKEN)
+    solver = EasyBinarioSolver()
+    submitter = PuzzleBinarioSubmitter(API_TOKEN)
 
     puzzle = "binairo-8x8-easy/"
     rows = 8
     cols = 8
 
-    auto_solver.run(solver, puzzle, rows, cols)
+    submitter.run(solver, puzzle, rows, cols)
